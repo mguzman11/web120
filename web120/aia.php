e19c951c0f77f5877a86ed7408c2e25996c8966c
Melissa Guzman <52518338+mguzman11@users.noreply.github.com>
2020-03-23 15:35:11 -0700

    completed aia

--- web120/aia.php
+++ web120/aia.php
@@ -1,12 +1,88 @@
 <?php include 'includes/header.php'?>
-<p>The info will go here</p>
+<h2>Baby Lestina</h2>
+<table>
+    <thead>
+		<tr>
+			    	<td><strong>Client Name and Information</strong></td>
+			        <td><strong>Website Information</strong></td>
+			        <td><strong>Client's Goals</strong></td>
+		    	</tr>
+			</thead>
+			<tbody>
+				<tr>
+                    <td>
+                        <ul>
+                            <li>Jack and Gabi Lestina</li>
+                            <li>The Lestina's are expecting their first child. Neither of them have family in the area, but feel
+                                strongly about connecting with family and friends during this time of pregnacy. Jack and Gabi want their
+                                families to feel connected and involved in their experience of becoming new parents. </li>
+                        </ul>
+                    </td>
+			        <td>
+                        <ul>
+                            <li>This website will serve as a virtual gathering space for friends and family to connect with the Lestinas.</li>
+                            <li>This is a new website with the host name of <a href="babylestina.com" target="_blank"> babylestina.com </a></li>
+                            <li>BabyLestina.com is an informtional website with external links to baby resources and their registry</li>
+                        </ul>
+                       <td> 
+                        <ul>
+			        		<li>A site for friends and family to access and actively participate in Jack and Gabi's preparation of becoming new parents</li>
+			        	</ul>
+			        </td>
+			    </tr>
+			</tbody>
+		</table>
+
+<table>
+			<thead>
+				<tr>
+			    	<td><strong>Audience</strong></td>
+			        <td><strong>Issues, Concerns, and Needs</strong></td>
+			        <td><strong>Approach</strong></td>
+		    	</tr>
+			</thead>
+			<tbody>
+				<tr>
+                    <td>
+                        <ul>
+                            <li>Family and Friends</li>
+                            <li>Anyone who wants to see and participate in the coming of Baby Lestina </li>
+                            <li>This website will be a one stop shop for baby Lestina</li>
+                        </ul>
+                    </td>
+			        <td>
+                        <ul>
+                            <li>With the growing concern of COVID-19, social distancing is strongly encouraged. This website is a place for Gabi and Jack to virtually interact with family and friends who are eagerly anticipating the coming of Baby Lestina</li>
+                            <li>There is will be online/virtual baby shower games available for people to participate in</li>
+                            <li>This website will serve as a gathering place of all things related to Baby Lestina</li>
+			        		<li>Baby growth and information</li>
+			        		<li>Place to offer encouragement and participate in preparation of baby Lestina</li>
+			        		<li>Access baby registery</li>
+			        		<li>See latest pictures of the baby</li>
+			        	</ul>
+			        </td>
+			        <td>
+			        	<ul>
+			        		<li>Links to Gabi and Jack's favortie resources</li>
+			        		<li>Links to baby registery</li>
+			        		<li>Online baby games/baby shower</li>
+			        		<li>Picture gallary</li>
+			        		<li>Contact form for encouragement and baby advice</li>
+			        	</ul>
+			        </td>
+			    </tr>
+			</tbody>
+		</table>
+		
+
 </section>
 <!-- END LEFT COL -->
 
 <!-- START RIGHT COL -->
 <aside>
- <h3>More Info</h3>
- <p>It will go here</p>
+    <h2>The Lestinas</h2>
+    <img src="images/jackandgabi.jpg" alt="Tall, white hipster man and average, white hipster woman"/>
+
 </aside>
 <!-- END RIGHT COL -->
  
